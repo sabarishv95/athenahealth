c34902ddf06d52ed1a826877afdfda2a9a73820e
sabarishv95 <sabarish.messi@gmail.com>
2021-09-11 23:44:54 +0530

    added constants

---
+++ client/src/modules/PatientBillingDetails/constants/index.js
@@ -0,0 +1,43 @@
+export const SALUTATIONS = [
+  {
+    id: 1,
+    label: "Mr.",
+    name: "mister",
+  },
+  {
+    id: 2,
+    label: "Ms.",
+    name: "miss",
+  },
+  {
+    id: 3,
+    label: "Mrs.",
+    name: "missus",
+  },
+];
+
+export const AGE_TYPE = [
+  {
+    id: 1,
+    label: "Years",
+    name: "years",
+  },
+  {
+    id: 2,
+    label: "Months",
+    name: "months",
+  },
+  {
+    id: 3,
+    label: "Days",
+    name: "days",
+  },
+];
+
+export const COUNTRY = [
+  {
+    id: 1,
+    label: "India",
+    name: "india",
+  },
+];
