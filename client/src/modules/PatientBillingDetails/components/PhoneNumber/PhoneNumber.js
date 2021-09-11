bd1e7dd2bd73c5d5e76cc786af496fde91c1f535
sabarishv95 <sabarish.messi@gmail.com>
2021-09-11 14:28:44 +0530

    removed unused imports

--- client/src/modules/PatientBillingDetails/components/PhoneNumber/PhoneNumber.js
+++ client/src/modules/PatientBillingDetails/components/PhoneNumber/PhoneNumber.js
@@ -1,13 +1,8 @@
 import React from "react";
 import Input from "../../../../commons/components/form/Input/Input";
-import Label from "../../../../commons/components/form/Label";
 
 function PhoneNumber() {
-  return (
-    <>
-      <Input id="phNo" type="number" width="200" height="34" />
-    </>
-  );
+  return <Input id="phNo" type="number" width="200" height="34" />;
 }
 
 export default React.memo(PhoneNumber);
