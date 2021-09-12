6024deaeda90ef1eec4672f175e38e8c1fe9e123
sabarishv95 <sabarish.messi@gmail.com>
2021-09-12 20:04:55 +0530

    fixed resolver

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,9 +3,37 @@ const Scans = require("../models/scans/scans.model");
 const Transactions = require("../models/transactions/transactions.model");
 
 module.exports = {
-  createAppointment: function (data) {
-    const appointment = Appointments.create(data.appointment);
-    return appointment;
+  createAppointment: async function (data) {
+    let { appointment } = data;
+    let createdAppointment;
+    const scanIds = [];
+    return new Promise((resolve) => {
+      appointment.medicalScanDetails.forEach((scan, index) => {
+        delete scan.Sno;
+        Scans.create(scan)
+          .then((response) => {
+            scanIds.push(response._id);
+            if (index + 1 === appointment.medicalScanDetails.length) {
+              resolve(response);
+            }
+          })
+          .catch((error) => {
+            resolve(error);
+          });
+      });
+    }).then(async (res) => {
+      delete appointment.medicalScanDetails;
+      appointment = {
+        ...appointment,
+        medicalScanDetails: scanIds,
+      };
+      createdAppointment = await Appointments.create(appointment);
+      createdAppointment = await createdAppointment
+        .populate("medicalScanDetails")
+        .populate("transactions")
+        .execPopulate();
+      return JSON.parse(JSON.stringify(createdAppointment));
+    });
   },
   getAppointments: function () {
     const appointments = Appointments.find(data)
