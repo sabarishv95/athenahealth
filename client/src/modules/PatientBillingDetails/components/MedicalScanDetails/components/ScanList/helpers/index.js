02177ce599dba27926a8709b0cbb76c44e328328
sabarishv95 <sabarish.messi@gmail.com>
2021-09-12 18:19:43 +0530

    added JsDoc

--- client/src/modules/PatientBillingDetails/components/MedicalScanDetails/components/ScanList/helpers/index.js
+++ client/src/modules/PatientBillingDetails/components/MedicalScanDetails/components/ScanList/helpers/index.js
@@ -1,5 +1,10 @@
 import { MEDICAL_BILLING_MASTER } from "../../../../../constants";
 
+/**
+ * 
+ * @param {String} medicalBilling 
+ * @returns medical billing {Object}
+ */
 export function getMedicalBilling(medicalBilling) {
   return MEDICAL_BILLING_MASTER.find((obj) => obj.medicalBilling === medicalBilling);
 }
@@ -13,6 +18,12 @@ export function getAmount(medicalBilling) {
   return billing ? billing.scanAmount : null;
 }
 
+/**
+ * 
+ * @param {String} medicalBilling 
+ * @param {Number} discount 
+ * @returns if the provided disscount is valid or not
+ */
 export function validateDiscount(medicalBilling, discount) {
   const billing = getMedicalBilling(medicalBilling);
   return discount <= billing.maxDiscount.amount;
