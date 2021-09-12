eb58a91d3aa2c85e05e98f5d173baef5c3967ce0
sabarishv95 <sabarish.messi@gmail.com>
2021-09-12 20:03:56 +0530

    removed unwanted packages and fixed API calls

--- client/src/API/index.js
+++ client/src/API/index.js
@@ -1,49 +1,18 @@
-import { ApolloClient } from "apollo-client";
-import { HttpLink } from "apollo-link-http";
-import { InMemoryCache } from "apollo-cache-inmemory";
-import gql from "graphql-tag";
-
-const defaultOptions = {
-  watchQuery: {
-    fetchPolicy: "network-only",
-    errorPolicy: "ignore",
-  },
-  query: {
-    fetchPolicy: "network-only",
-    errorPolicy: "all",
-  },
-};
-
-const client = new ApolloClient({
-  link: new HttpLink({ uri: "/graphql" }),
-  cache: new InMemoryCache(),
-  defaultOptions: defaultOptions,
-});
+import axios from "axios";
+const endPointUrl = "/graphql";
 
 class Api {
-  queryRequest(query, data) {
+  graphqlRequest(mutation, data) {
     return new Promise((resolve, reject) => {
-      client
-        .query({
-          query: gql`
-            ${query}
-          `,
+      axios({
+        method: "POST",
+        headers: { "content-type": "application/json" },
+        url: endPointUrl,
+        data: JSON.stringify({
+          query: mutation,
           variables: { ...data },
-        })
-        .then((data) => resolve(data))
-        .catch((error) => reject(error));
-    });
-  }
-
-  mutationRequest(mutation, data) {
-    return new Promise((resolve, reject) => {
-      client
-        .mutate({
-          mutation: gql`
-            ${mutation}
-          `,
-          variables: { ...data },
-        })
+        }),
+      })
         .then((data) => resolve(data))
         .catch((error) => reject(error));
     });
